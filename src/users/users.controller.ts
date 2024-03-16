158761f28e93439e96742ac31ce0871f422686aa
juanQuiroz <jaqp_911@hotmail.com>
2024-03-16 13:48:32 -0500

    Debug test - endpoint register-user

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -17,7 +17,7 @@ import { UpdateUserDto } from './dto/update-user.dto';
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @Post('register')
+  @Post('register-user')
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
